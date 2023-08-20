776ae99b1b3bb5945d11d94b1d361bf121682217
arleyxD <castearley@gmail.com>
2023-08-20 14:22:05 -0500

    [+] se agrega repositoy jpa y se crea el modelo de la transaccion

--- springboot-credibanck-app/src/main/java/model/Transaction.java
+++ springboot-credibanck-app/src/main/java/model/Transaction.java
@@ -1,5 +1,52 @@
 package model;
 
+import java.util.Date;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Transaction {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long transactionId;
+	private String cardId;
+	private Date transactionDate;
+	private double TransactionAmount;
+	private String transactionType;
+	public Long getTransactionId() {
+		return transactionId;
+	}
+	public void setTransactionId(Long transactionId) {
+		this.transactionId = transactionId;
+	}
+	public String getCardId() {
+		return cardId;
+	}
+	public void setCardId(String cardId) {
+		this.cardId = cardId;
+	}
+	public Date getTransactionDate() {
+		return transactionDate;
+	}
+	public void setTransactionDate(Date transactionDate) {
+		this.transactionDate = transactionDate;
+	}
+	public double getTransactionAmount() {
+		return TransactionAmount;
+	}
+	public void setTransactionAmount(double transactionAmount) {
+		TransactionAmount = transactionAmount;
+	}
+	public String getTransactionType() {
+		return transactionType;
+	}
+	public void setTransactionType(String transactionType) {
+		this.transactionType = transactionType;
+	}
+	
+	
 }
--- springboot-credibanck-app/src/main/java/repository/CardRepository.java
+++ springboot-credibanck-app/src/main/java/repository/CardRepository.java
@@ -1,5 +1,10 @@
 package repository;
 
-public class CardRepository {
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import model.Card;
+
+public interface CardRepository extends JpaRepository<Card, String> {
+    // Métodos personalizados de consulta si es necesario
 }
