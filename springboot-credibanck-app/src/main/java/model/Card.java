3d8091f5973099ef640026d395629132e3b0c81c
arleyxD <castearley@gmail.com>
2023-08-20 14:00:19 -0500

    [+] Se crea la clase Card

--- springboot-credibanck-app/src/main/java/model/Card.java
+++ springboot-credibanck-app/src/main/java/model/Card.java
@@ -1,5 +1,62 @@
 package model;
 
+import java.util.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Card {
 
+	@Id
+	private String cardId;
+	private String productId;
+	private String cardNumber;
+	private String cardHolderName;
+	private Date expirationDate;
+	private boolean isActive;
+	private double balance;
+	
+	
+	public String getCardId() {
+		return cardId;
+	}
+	public void setCardId(String cardId) {
+		this.cardId = cardId;
+	}
+	public String getProductId() {
+		return productId;
+	}
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+	public String getCardNumber() {
+		return cardNumber;
+	}
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+	public String getCardHolderName() {
+		return cardHolderName;
+	}
+	public void setCardHolderName(String cardHolderName) {
+		this.cardHolderName = cardHolderName;
+	}
+	public Date getExpirationDate() {
+		return expirationDate;
+	}
+	public void setExpirationDate(Date expirationDate) {
+		this.expirationDate = expirationDate;
+	}
+	public boolean isActive() {
+		return isActive;
+	}
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
 }
