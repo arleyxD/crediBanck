dc5cc873588998ba563d4025c23156a5e498ad33
arleyxD <castearley@gmail.com>
2023-08-21 21:02:24 -0500

    Se realiza primera prueba del servicio, se agrega propiedad para que genere la tabla con la base de datos

--- springboot-credibanck-app/src/main/java/com/springboot/app/model/Card.java
+++ springboot-credibanck-app/src/main/java/com/springboot/app/model/Card.java
@@ -1,10 +1,16 @@
 package com.springboot.app.model;
 
 import java.util.Date;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "card")
 public class Card {
 
 	@Id
